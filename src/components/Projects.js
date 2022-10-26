f1f6a08ce5639d4733f3923852abadf2bef5f9e7
jack <jackmccalley@gmail.com>
2022-10-26 09:40:53 -0700

    vercel upload

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,44 @@
 import React from "react";
 import examplephoto128 from "../images/examplephoto128.png";
-import p3dexamplephoto1 from '../images/p3dexamplephoto1.png'
-import Project from './Project'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import p3dexamplephoto1 from "../images/p3dexamplephoto1.png";
+import Project from "./Project";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "../themes/Projects.css";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
 export default function Projects() {
-  var description128 = "128 Productions is an events coordination company located in Denver, CO. Written in React.js. Incorporated Contentful CMS for event management and photo galleries. Displays a developed and advanced understanding of React Hooks and headless content management systems."
-  var descriptionp3d = "Precision Digital Dental Designs was a Dental startup company that specialized in Surgical Guides and 3D Modeling. Developed in ReactJS on Rails with AWS and Shopify integration. Uses a RESTful Rails API for user data and file management, Shopify for payment, and AWS for file storage."
+  var description128 =
+    "128 Productions is an events coordination company located in Denver, CO. Written in React.js. Incorporated Contentful CMS for event management and photo galleries. Displays a developed and advanced understanding of React Hooks and headless content management systems.";
+  var descriptionp3d =
+    "Precision Digital Dental Designs was a Dental startup company that specialized in Surgical Guides and 3D Modeling. Developed in ReactJS on Rails with AWS and Shopify integration. Uses a RESTful Rails API for user data and file management, Shopify for payment, and AWS for file storage.";
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Project image={examplephoto128} title='128 Productions' description={description128} link="https:www.128productions.com" target="_blank" />
-      <Project image={p3dexamplephoto1} title='P3D Guides' description={descriptionp3d} link="https://p3d-frontend.herokuapp.com/" target="_blank"/>
-    </ThemeProvider>
+    <div>
+      <h1 className="projects-title">Past Projects</h1>
+      <ThemeProvider theme={darkTheme}>
+        <Project
+          image={examplephoto128}
+          title="128 Productions"
+          description={description128}
+          link="https:www.128productions.com"
+          target="_blank"
+        />
+        <Project
+          image={p3dexamplephoto1}
+          title="P3D Guides"
+          description={descriptionp3d}
+          link="https://p3d-frontend.herokuapp.com/"
+          target="_blank"
+        />
+        <form method="post" action="">
+          <input type="text" required/>
+          <button type="submit">Submit</button>
+        </form>
+      </ThemeProvider>
+    </div>
   );
 }
